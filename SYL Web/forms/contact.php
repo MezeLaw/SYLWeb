eae0b9d17fcbb4eb69db7d750a682276d8d08347
Meze <mezelaw@gmail.com>
2022-03-25 23:03:07 -0300

    Cleaning contact.php.

--- SYL Web/forms/contact.php
+++ SYL Web/forms/contact.php
@@ -1,14 +1,11 @@
 <?php
  
-
-//$remitente = "mezequielabogado@gmail.com";
-$destinatario = "info@syl.com.ar";
-//$destinatario = "info@enviofacil.com.ar"; 
-
-$name= $_POST['name'];
+ 
+$destinatario = "info@syl.com.ar"; 
+ 
 $email= $_POST['email'];
 $subject= $_POST['subject'];
-$message= $_POST['message'];
+$message= "Mensaje enviado por: " . $_POST['name'] . "\n \nMensaje: \n" . $_POST['message'];
 
 $headers = "From: $email\r\nReply-to: $email";
 
